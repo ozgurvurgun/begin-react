bb99f17eec8e220e08c5a3f07345f22715d65053
Ozgur VURGUN <ozgurvurgun1@gmail.com>
2022-11-27 22:06:23 +0300

    JSX attributes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const names = ["ozgur", "ece", "elis"];
+
+//<button style="color:white;padding: 10px;background-color:red;border:2px solid yellow;" type="button">HTML BUTTON</button>
 
 function App() {
-    return(
-    <div>
-        {names.map(name => (<h1>{name}</h1>))}
-    </div>
+    const buttonValue = "Benim Button";
+    const str1 = "Click ";
+    const str2 = "Me";
+
+    return (
+        <div>
+            <button tabIndex={3} className='jsxCLASS' type="button" style={{ padding: '10px', color: 'white', backgroundColor: 'blue', border: '2px solid yellow' }}>{str1.concat(str2)}</button>
+        </div>
     );
 }
 
